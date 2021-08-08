4d90684051cbdbfcd5d334b9ce496e977ff7345d
MohamedB <m.m.bassiouni@gmail.com>
2021-08-07 20:02:32 -0700

    setting up server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+
+// routes
+app.use(require("./routes/api.js"));
+app.use(require('./routes/homeRoutes.js'))
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
